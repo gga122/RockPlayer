d90886579ceb4b4cbb95ea05355efd1db79c9f3f
Henry <zhy289044312@gmail.com>
2019-08-02 00:28:39 +0800

    [add] add media player files

---
+++ RockPlayer/Public Header/RockMediaPlayer.h
@@ -0,0 +1,18 @@
+//
+//  RockMediaPlayer.h
+//  RockPlayer
+//
+//  Created by Henry on 2019/8/2.
+//  Copyright © 2019 Mickyzhu. All rights reserved.
+//
+
+#ifndef RockMediaPlayer_h
+#define RockMediaPlayer_h
+
+#include "RockBase.h"
+
+typedef const struct RockMediaPlayer * RockMediaPlayerRef;
+
+
+
+#endif /* RockMediaPlayer_h */
---
+++ RockPlayer/Source/RockMediaPlayer.c
@@ -0,0 +1,13 @@
+//
+//  RockMediaPlayer.c
+//  RockPlayer
+//
+//  Created by Henry on 2019/8/2.
+//  Copyright © 2019 Mickyzhu. All rights reserved.
+//
+
+#include "RockMediaPlayer.h"
+
+struct RockMediaPlayer {
+    
+};
