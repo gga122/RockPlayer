d397eaa73e996feb6937dd9584a92a640b37c181
Henry <zhy289044312@gmail.com>
2019-08-03 00:29:10 +0800

    [add] add OpenGL display output

---
+++ RockPlayer/Public Header/RockDisplayOutputOpenGL.h
@@ -0,0 +1,16 @@
+//
+//  RockDisplayOutputOpenGL.h
+//  RockPlayer
+//
+//  Created by Henry on 2019/8/3.
+//  Copyright © 2019 Mickyzhu. All rights reserved.
+//
+
+#ifndef RockDisplayOutputOpenGL_h
+#define RockDisplayOutputOpenGL_h
+
+#include "RockBase.h"
+
+
+
+#endif /* RockDisplayOutputOpenGL_h */
---
+++ RockPlayer/Source/RockDisplayOutputOpenGL.c
@@ -0,0 +1,10 @@
+//
+//  RockDisplayOutputOpenGL.c
+//  RockPlayer
+//
+//  Created by Henry on 2019/8/3.
+//  Copyright © 2019 Mickyzhu. All rights reserved.
+//
+
+#include "RockDisplayOutputOpenGL.h"
+
